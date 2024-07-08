1c1565462ff67121452a4048a6386bc1e6d66752
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-08 12:08:37 +0600

    Section 2: 20. Variables

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,30 +4,22 @@ class Sample
 {
     static void Main() // Return type of Main method  should be (void or int) and it should be static
     {
-        // System is a predefined Namespace which is a part of BCL.
-        // Inside that Namespace, we have Console Class. 
-        // Inside Console Class, we have predefined static 'WriteLine' method.
-        System.Console.WriteLine("Hello");
-       // System.Console.ReadKey(); // hey console, wait for the user until user press any key on the keyboard.
+        //declare the variable
+        int a = 100;
+        //print value
+        System.Console.WriteLine(a);
+     
 
+        string studentName = "Khalid";
+        int age = 20;
 
-        //Print messages line-by-line
-        System.Console.WriteLine("Welcome");
-        System.Console.WriteLine("to");
-        System.Console.WriteLine("C# Programming");
+        //print value
+        System.Console.Write("Hey");
+        System.Console.Write(studentName);
+        System.Console.Write(". your age is");
+        System.Console.Write(age);
 
-        //Print messages side-by-side
-        System.Console.Write("Welcome");
-        System.Console.Write(" to ");
-        System.Console.Write("C# Programmiong");
-
-        //Wait for pressing some key on keyboard
+        //wait for pressing some key
         System.Console.ReadKey();
-
-        // clear the screen
-        System.Console.Clear();
-
-        System.Console.WriteLine("Thank you");
-        Console.ReadKey();
     }
 }
