3d881d850a3998ec7dbb26b028228904e2840008
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-08 18:29:01 +0600

    Break

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,18 +4,11 @@ class Sample
 {
     static void Main()
     {
-        //1 to 10
-        for (int i = 1; i <= 10; i++)
+        for(int i = 0; i < 10; i++)
         {
-            Console.WriteLine(i);
+            if (i == 5) break;
+            System.Console.WriteLine(i);
         }
-
-        // 9 to 0
-        for (int i = 9; i >= 0; i--)
-        {
-            Console.WriteLine(i);
-        }
-
         System.Console.ReadKey();
     }
 }
