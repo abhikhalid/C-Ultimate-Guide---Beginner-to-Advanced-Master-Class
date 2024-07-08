8acf7c878f3fd565c4b968aea7fc1c7ac7e6c2a6
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-08 18:22:17 +0600

    For Loop

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,28 +4,17 @@ class Sample
 {
     static void Main()
     {
-        // 1 to 10
-        int i = 1;
-        while (i <= 10)
+        //1 to 10
+        for (int i = 1; i <= 10; i++)
         {
-            System.Console.WriteLine(i + "");
-            i++;
+            Console.WriteLine(i);
         }
 
         // 9 to 0
-        i = 9;
-        while (i >= 0)
+        for (int i = 9; i >= 0; i--)
         {
-            System.Console.WriteLine(i + "");
-            i--;
+            Console.WriteLine(i);
         }
-
-        i = 9;
-        do 
-        {
-            System.Console.WriteLine(i + "");
-            i--;
-        }while (i >= 0);
 
         System.Console.ReadKey();
     }
