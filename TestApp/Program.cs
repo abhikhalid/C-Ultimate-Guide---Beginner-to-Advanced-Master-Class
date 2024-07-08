3c1717c20ee5d0fb059d0fdf4714422b7fe7558c
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-08 13:03:25 +0600

    Section 2 : 22. Primitive Types.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,22 +4,41 @@ class Sample
 {
     static void Main() // Return type of Main method  should be (void or int) and it should be static
     {
-        //declare the variable
-        int a = 100;
-        //print value
+        sbyte a = sbyte.MaxValue;
+        sbyte b = 100;
+        sbyte c = default(sbyte);
+
+        byte d = byte.MaxValue;
+        short e = short.MaxValue;
+        ushort f = ushort.MaxValue;
+        int g = int.MaxValue;
+        uint h = uint.MaxValue;
+        long i = long.MaxValue;
+        ulong j = ulong.MaxValue;
+        float k=float.MaxValue;
+        double l = double.MaxValue;
+        decimal m = decimal.MaxValue;
+        char n = 'A';
+        string o = "abc";
+        bool p = true;
+
         System.Console.WriteLine(a);
-     
+        System.Console.WriteLine(b);
+        System.Console.WriteLine(c);
+        System.Console.WriteLine(d);
+        System.Console.WriteLine(e);
+        System.Console.WriteLine(f);
+        System.Console.WriteLine(g);
+        System.Console.WriteLine(h);
+        System.Console.WriteLine(i);
+        System.Console.WriteLine(j);
+        System.Console.WriteLine(k);
+        System.Console.WriteLine(l);
+        System.Console.WriteLine(m);
+        System.Console.WriteLine(n);
+        System.Console.WriteLine(o);
+        System.Console.WriteLine(p);
 
-        string studentName = "Khalid";
-        int age = 20;
-
-        //print value
-        System.Console.Write("Hey");
-        System.Console.Write(studentName);
-        System.Console.Write(". your age is");
-        System.Console.Write(age);
-
-        //wait for pressing some key
         System.Console.ReadKey();
     }
 }
