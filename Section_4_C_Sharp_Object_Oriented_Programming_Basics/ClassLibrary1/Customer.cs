af193aa23f8caeb9f262f059d3b0191ded272f8e
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-09 12:17:12 +0600

    Creating Classes & Objects

---
+++ Section_4_C_Sharp_Object_Oriented_Programming_Basics/ClassLibrary1/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// The 'Class Library' is a collection of classes that will be compiled as a 'dll' (Dynamic Link Library) file and that 'dll' file reference can be added
+// to the console application. The the console application can access any class that is present inside the class libary.
+namespace ClassLibrary1
+{
+    public class Customer
+    {
+        public int customerID;
+        public string customerName;
+    }
+}
---
+++ Section_4_C_Sharp_Object_Oriented_Programming_Basics/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+
+public class Sample
+{
+    public static void Main()
+    {
+        //reference variables
+        Customer c1, c2;
+
+        //Objects
+        c1 = new Customer();
+        c2 = new Customer();
+    }
+}
