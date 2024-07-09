0d1cda6b388a8ce6ae884d2f3a59077e62c48f8f
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-09 09:51:24 +0600

    Creating Customers and Accounts Menu

--- Section_3_Bank_Project_Getting_Started/Program.cs
+++ Section_3_Bank_Project_Getting_Started/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Security.Principal;
+
+class Program
 {
     //Application execution starts here
     static void Main()
@@ -42,8 +44,10 @@
                 switch (mainMenuChoice)
                 {
                     case 1: // TO DO: Display Customers menu
+                        CustomersMenu();
                         break;
                     case 2: // TO DO: Display Accounts menu
+                        AccountsMenu();
                         break;
                     case 3: // TO DO: Display Fund Transfer menu
                         break;
@@ -62,5 +66,48 @@
         //about to exit
         System.Console.WriteLine("Thank you! Visit again.");
         System.Console.ReadKey();
+    }
+
+    static void CustomersMenu()
+    {
+        //variable to store customers menu choice
+        int customerMenuChoice = -1;
+        do
+        {
+            //print customers menu
+            System.Console.WriteLine("\n:::Customers menu:::");
+            System.Console.WriteLine("1. Add Customer");
+            System.Console.WriteLine("2. Delete Customer");
+            System.Console.WriteLine("3. Update Customer");
+            System.Console.WriteLine("4. View Customers");
+            System.Console.WriteLine("0. Back to Main Menu");
+
+            //accept customers menu choice
+            System.Console.Write("Enter choice:");
+            customerMenuChoice = int.Parse(System.Console.ReadLine());
+            
+        }while (customerMenuChoice != 0);
+ 
+    }
+
+    static void AccountsMenu()
+    {
+        //variable to store accounts menu choice
+        int accountsMenuChoice = -1;
+        do
+        {
+            //print customers menu
+            System.Console.WriteLine("\n:::Accounts menu:::");
+            System.Console.WriteLine("1. Add Account");
+            System.Console.WriteLine("2. Delete Account");
+            System.Console.WriteLine("3. Update Account");
+            System.Console.WriteLine("4. View Account");
+            System.Console.WriteLine("0. Back to Main Menu");
+
+            //accept accounts menu choice
+            System.Console.Write("Enter choice:");
+            accountsMenuChoice = int.Parse(System.Console.ReadLine());
+
+        } while (accountsMenuChoice != 0);
     }
 }
