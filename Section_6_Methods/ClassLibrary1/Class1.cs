9ebfebbcc5abd66774300f5a1c2c74708fdbb408
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-09 16:35:01 +0600

    Understanding Methods

---
+++ Section_6_Methods/ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿// The Class Libaray will be compiled as DLL file.
+
+//It's a good way of programming style to declare all the resuable classes inside the class library; instead of writing them in the console application.
+// by adding reference of the Class Library to the Console Application, it will add reference of the compiled file of class libaray into the console application project.
+public class Product
+{
+    //fields
+    //public int productID=1001; (not good way)
+    public int productID;
+    public string productName;
+    public double cost;
+    public double tax;
+    public int quantityInStock;
+    public static int TotalNoProducts;
+    public const string CategoryName = "Computer";
+    public readonly string dateofPurchase;
+
+    public Product()
+    {
+        dateofPurchase = System.DateTime.Now.ToShortDateString();
+    }
+
+    public void Method()
+    {
+        TotalNoProducts = 0;
+    }
+
+    // method
+    // cost <=2000 then tax = 10%
+    // cost > 20000 then tax = 12.5%
+    public void CalculateTax()
+    {
+        //create local variable
+        double t;
+        //calculate tax
+        if(cost <= 2000)
+        {
+            t = cost * 10 / 1000;
+        }
+        else
+        {
+            t = cost * 12.5 / 1000;
+        }
+        tax = t;
+    }
+}
---
+++ Section_6_Methods/Program.cs
@@ -0,0 +1,77 @@
+﻿class Sample
+{
+    public static void Main()
+    {
+        // Local Constant
+        const string developerName = "khalid";
+
+        //create reference variables
+        Product product1, product2, product3;
+
+        // create objects
+        product1 = new Product();
+        Product.TotalNoProducts++; //1
+        product2 = new Product();
+        Product.TotalNoProducts++; //2 
+        product3 = new Product();
+        Product.TotalNoProducts++; //3
+
+        //initialize fields
+        product1.productID = 1001;
+        product1.productName = "Mobile";
+        product1.cost = 20000;
+        product1.quantityInStock = 1200;
+
+        product2.productID = 1002;
+        product2.productName = "Laptop";
+        product2.cost = 45000;
+        product2.quantityInStock = 3400;
+
+        product3.productID = 1003;
+        product3.productName = "Speakers";
+        product3.cost = 36000;
+        product3.quantityInStock = 800;
+
+        //call methods
+        product1.CalculateTax();
+        product2.CalculateTax();
+        product3.CalculateTax();
+
+        //get values from fields
+        System.Console.WriteLine(developerName);
+
+        System.Console.WriteLine("Product 1:");
+        System.Console.WriteLine("Product ID: " + product1.productID);
+        System.Console.WriteLine("Product Name: " + product1.productName);
+        System.Console.WriteLine("Product Cost: " + product1.cost);
+        System.Console.WriteLine("Quantity in Stock: " + product1.quantityInStock);
+        System.Console.WriteLine("Date of Purchase: " + product1.dateofPurchase);
+        System.Console.WriteLine("Tax: " + product1.tax);
+
+        System.Console.WriteLine("Product 2:");
+        System.Console.WriteLine("Product ID: " + product2.productID);
+        System.Console.WriteLine("Product Name: " + product2.productName);
+        System.Console.WriteLine("Product Cost: " + product2.cost);
+        System.Console.WriteLine("Quantity in Stock: " + product2.quantityInStock);
+        System.Console.WriteLine("Date of Purchase: " + product2.dateofPurchase);
+        System.Console.WriteLine("Tax: " + product2.tax);
+
+        System.Console.WriteLine("Product 3:");
+        System.Console.WriteLine("Product ID: " + product3.productID);
+        System.Console.WriteLine("Product Name: " + product3.productName);
+        System.Console.WriteLine("Product Cost: " + product3.cost);
+        System.Console.WriteLine("Quantity in Stock: " + product3.quantityInStock);
+        System.Console.WriteLine("Date of Purchase: " + product3.dateofPurchase);
+        System.Console.WriteLine("Tax: " + product3.tax);
+
+        int totalQuantity = product1.quantityInStock + product2.quantityInStock + product3.quantityInStock;
+        System.Console.WriteLine("Total Quantity", totalQuantity);
+        System.Console.WriteLine("Total no. of products:" + Product.TotalNoProducts); // 3
+        System.Console.WriteLine("Category of products:" + Product.CategoryName); // Output: Computers
+
+
+        //TO DO: Find out the highes cost of the three product
+
+        System.Console.ReadKey();
+    }
+}
