15a80aa07cfd452f5915c25bb3ef92ae0825ce0c
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-09 09:27:02 +0600

    Creating Login Form

---
+++ Section_2_C_Sharp_Language_Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Sample
+{
+    static void Main()
+    {
+        System.Console.WriteLine("USA");
+        System.Console.WriteLine("UK");
+        System.Console.WriteLine("India");
+        goto mylabel;
+        System.Console.WriteLine("France");
+        System.Console.WriteLine("Italy");
+        System.Console.WriteLine("Iran");
+        mylabel:
+        System.Console.WriteLine("Nepal");
+        System.Console.WriteLine("Dubai");
+       
+        System.Console.ReadKey();
+    }
+}
