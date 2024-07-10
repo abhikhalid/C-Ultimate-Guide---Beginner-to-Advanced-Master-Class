530a9d5d6e7f263cb48a521a4df23a675c57304d
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-10 12:00:43 +0600

    Parameter Modifiers - params

---
+++ ParamsExample/Program.cs
@@ -0,0 +1,23 @@
+﻿class Student
+{
+    public void DisplaySubjects(params string[] subjects)
+    {
+        for(int i = 0;i < subjects.Length;i++)
+        {
+            System.Console.WriteLine(subjects[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //create object of Student class
+        Student s = new Student();
+        //access DisplaySubjects method
+        s.DisplaySubjects("Theory of Computation", "Computer Networks", "Discrete Mathematics", "Digital System Design");
+
+        System.Console.ReadKey();
+    }
+}
