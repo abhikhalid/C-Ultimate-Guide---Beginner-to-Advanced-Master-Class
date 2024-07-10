bdfd19278b8310e094bddab9c71515bc34ed28b8
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-10 21:22:49 +0600

    Constructor Overloading

--- Section_8_Constructors/Section_8_Constructors/ClassLibrary1/Class1.cs
+++ Section_8_Constructors/Section_8_Constructors/ClassLibrary1/Class1.cs
@@ -8,18 +8,24 @@
     public static string companyName;
 
     //constructor
-    public Employee()
+    public Employee() // default/ parameter-less constructor
     {
         empID = 101;
         empName = "No name";
         job = "some job title";
     }
 
-    public Employee(int empID, string empName, string job)
+    public Employee(int empID, string empName, string job) //parameterized constructor
     {
         this.empID = empID;
         this.empName = empName;
         this.job = job;
+    }
+
+    public Employee(int empID, string empName) //parameterized constructor
+    {
+        this.empID = empID;
+        this.empName = empName;
     }
 
     // static constructor is public by default. 
