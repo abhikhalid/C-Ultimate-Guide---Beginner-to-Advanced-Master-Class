a6417979d7625d204dd636f0923939184d330bcc
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-10 14:53:30 +0600

    Section 7 : Implicit Casting

---
+++ Section_7_Type_Conversion/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //sbyte
+        sbyte a = 10;
+
+        //int
+        int b;
+
+        //copy the value from 'a' to 'b'
+        b = a; // Implicit Casting
+
+        System.Console.WriteLine(a);
+        System.Console.WriteLine(b);
+        System.Console.ReadKey();
+    }
+}
