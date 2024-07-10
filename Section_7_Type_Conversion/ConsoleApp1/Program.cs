caa6b766d58a93473323d23dd0fb28486c85f8bf
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-10 15:15:43 +0600

    Parse

--- Section_7_Type_Conversion/ConsoleApp1/Program.cs
+++ Section_7_Type_Conversion/ConsoleApp1/Program.cs
@@ -2,22 +2,10 @@
 {
     static void Main()
     {
-        //sbyte
-        int a = 500;
+        string a = "100";
+        int b =  int.Parse(a);
+        System.Console.WriteLine(b);
 
-        //float
-        float b;
-
-        //int to float
-        b = a;
-        b = (float)a; // Explicit Casting
-
-        byte c;
-        c = (byte)a; // loosy conversion
-
-        System.Console.WriteLine(a); 
-        System.Console.WriteLine(b); 
-        System.Console.WriteLine(c); 
         System.Console.ReadKey();
     }
 }
