4cdd75e7125cd0d88b826a07d7907e2265b520cc
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-12 16:17:46 +0600

    Creating Indexers

---
+++ Section_8_Constructors_and_Section_9_Properties_Indexers/Section_8__Section_9/IndeersExample/IndeersExample/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Car car = new Car();
+
+        //call get accesssor of indexer
+        System.Console.WriteLine(car[0]); // BMW
+
+        car[0] = "Nissan";
+
+        System.Console.ReadKey();
+    }
+}
