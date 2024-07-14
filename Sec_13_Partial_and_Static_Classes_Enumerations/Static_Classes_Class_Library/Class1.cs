3f2e38560f1cf644f53fd7010e921ec1d292a83a
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-14 13:20:45 +0600

    Static_Classes

---
+++ Sec_13_Partial_and_Static_Classes_Enumerations/Static_Classes_Class_Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿//Benefits: it's not possible to create instance member accidentaly.
+
+public static class Country
+{
+    public static string CountryName = "Bangladesh";
+    public static int NoOfStates = 28;
+
+    public static int GetNoOfUnionTerritories()
+    {
+        return 8;
+    }
+}
---
+++ Sec_13_Partial_and_Static_Classes_Enumerations/Static_Classes_Console_Application/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //create object of static class
+        //Country country = new Country(); // error because static class
+
+        //access static fields
+        System.Console.WriteLine(Country.CountryName);
+        System.Console.WriteLine(Country.NoOfStates);
+        System.Console.WriteLine(Country.GetNoOfUnionTerritories());
+
+        Console.ReadKey();
+    }
+}
