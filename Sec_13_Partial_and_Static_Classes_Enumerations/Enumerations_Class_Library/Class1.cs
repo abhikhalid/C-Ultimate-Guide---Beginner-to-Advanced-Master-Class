0333bdbe7412130ad3244e931ba71cef4a443697
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-14 13:53:35 +0600

    Sec_13_Note_Added

---
+++ Sec_13_Partial_and_Static_Classes_Enumerations/Enumerations_Class_Library/Class1.cs
@@ -0,0 +1,24 @@
+﻿public class Person
+{
+    public string PersonName { get; set; }
+    
+    public string Email { get; set; }
+
+    public AgeGroupEnumeration AgeGroup { get; set; } 
+}
+
+public enum AgeGroupEnumeration
+{
+    Child,
+    Teenager,
+    Adult = 100,
+    Senior
+}
+
+//public enum AgeGroupEnumeration:short
+//{
+//    Child,
+//    Teenager,
+//    Adult = 100,
+//    Senior
+//}
---
+++ Sec_13_Partial_and_Static_Classes_Enumerations/Enumerations_Console_App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //create object of Person class
+        Person person = new Person();
+        person.PersonName = "Khalid";
+        person.Email = "khalid@gmail.com";
+        person.AgeGroup = AgeGroupEnumeration.Adult;
+        //person.AgeGroup = AgeGroupEnumeration.Todler; //user is not allowed to set other value
+        
+        Console.WriteLine(person.AgeGroup);
+        Console.ReadKey();  
+    }
+}
