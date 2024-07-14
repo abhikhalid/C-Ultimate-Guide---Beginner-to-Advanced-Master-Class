7c0c468c74394593ba07b4408dd6799cb4f1b7a7
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-14 21:10:36 +0600

    Generic_Constranits

---
+++ Section_16_Generics/Generic_Constraints_Class_Library/Class1.cs
@@ -0,0 +1,27 @@
+﻿public abstract class Student
+{
+    //abstract property
+    public abstract int Marks { get; set; }
+}
+
+public class GraduateStudent : Student
+{
+    public override int Marks { get; set; }
+}
+
+public class PostGraduateStudent : Student
+{
+    public override int Marks { get; set; }
+}
+
+//Generic Class
+public class MarksPrinter<T> where T : Student
+{
+    public T stud;
+
+    public void PrintMarks()
+    {
+        Student temp = (Student)stud;
+        System.Console.WriteLine(temp.Marks);
+    }
+}
