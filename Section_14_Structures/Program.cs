8d21de2c13719690545ecfb140bcccf8a4128c10
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-14 15:03:09 +0600

    Creating_Structures

---
+++ Section_14_Structures/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //create structure instance
+        //Category category;
+
+        //initialize fields through properites
+
+        //showing error because of all the fields of the structure are not initialized.
+        
+        /*
+        category.CategoryID = 20;
+        category.CategoryName = "General";
+
+        */
+
+        // So it is must to initialize all the fields of the structure by calling the parameter-less consturctor
+        
+        //we are not creating any objects by using new keyword.
+        //Our intention is only to call the parameter-less constructor only to initialize of fields of the structure.
+        Category category = new Category();
+        category.CategoryID = 20;
+        category.CategoryName = "General";
+
+        //access methods
+        Console.WriteLine(category.CategoryID);
+        Console.WriteLine(category.CategoryName);
+        Console.WriteLine(category.GetCategoryNameLength());
+
+        Console.ReadKey();
+    }
+}
