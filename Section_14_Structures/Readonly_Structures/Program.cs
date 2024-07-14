65f3c86e624cd2f033997117fb0bdfd699e9a0a2
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-14 16:25:47 +0600

    Readonly_Structures

---
+++ Section_14_Structures/Readonly_Structures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Marvel m = new Marvel("IRON MAN");
+
+        Console.WriteLine(m.CharacterName);
+        m.PrintCharacterName();
+
+        Console.ReadKey();
+
+    }
+}
---
+++ Section_14_Structures/Readonly_Structures/Read_Only_Structures_Class_Library/Class1.cs
@@ -0,0 +1,24 @@
+﻿public readonly struct Marvel
+{
+    //field 
+    private readonly string _characterName;
+
+    //public
+    public string CharacterName
+    {
+        get { return _characterName; }
+    }
+
+    //public method
+    public void PrintCharacterName()
+    {
+        System.Console.WriteLine(this.CharacterName);
+        //this._characterName = "abc"; compile-time error
+    }
+
+    //constructor
+    public Marvel(string characterName)
+    {
+        this._characterName = characterName;
+    }
+}
