33c7dc0954ff72f39ebb15265ee03af8b386f695
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-14 10:16:36 +0600

    'Nested_Namespaces'

--- Section_12_Namespaces/Program.cs
+++ Section_12_Namespaces/Program.cs
@@ -4,6 +4,8 @@
     {
         HR.Manager manager;
         manager = new HR.Manager();
+
+        FrontOffice.CustomerEnquiry customerEnquiry = new FrontOffice.CustomerEnquiry();
         
     }
 }
