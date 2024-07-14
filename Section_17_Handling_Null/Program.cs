4291248cf138a9a4580e2112c1349a8287012da6
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-14 22:15:27 +0600

    Nullable_Types

---
+++ Section_17_Handling_Null/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Person
+{
+    public int? NoOfChildren;
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        //create object
+        Person p1 = new Person() { NoOfChildren = 1 };
+        Person p2 = new Person() { NoOfChildren = null };
+        Person p3 = null;
+
+
+        Console.WriteLine(p1.NoOfChildren);
+  
+        if(p2.NoOfChildren != null) //p2.NoOfChildren.HasValue
+        {
+            Console.WriteLine(p2.NoOfChildren);
+        }
+
+        Console.ReadKey();
+    }
+}
