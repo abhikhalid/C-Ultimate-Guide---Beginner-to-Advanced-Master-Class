bc74bf8161a22634e59685e0d2b87f4667e91cfa
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-15 15:37:20 +0600

    Inner Classes

---
+++ Section_18_Extension_Methods_and_Pattern_Matching/Inner_Classes_Class_Library/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inner_Classes_Class_Library
+{
+    public class Student
+    {
+        public double SecuredMarks { get; set; }
+        public double MaxMarks { get; set; }
+        public double Percentage { get; set; }
+    }
+
+    //outer class
+    public class MarksCalculation
+    {
+        public void CalculatePercentage(Student s)
+        {
+            //create object of inner class
+            CalculationHelper ch = new CalculationHelper();
+            s.Percentage = ch.Multipy(s.SecuredMarks / s.MaxMarks,100);
+        }
+
+        //inner class
+        public class CalculationHelper
+        {
+            public double Multipy(double n1, double n2)
+            {
+                return n1 * n2;
+            }
+        }
+    }
+}
---
+++ Section_18_Extension_Methods_and_Pattern_Matching/Inner_Classes_Console_App/Program.cs
@@ -0,0 +1,25 @@
+﻿using Inner_Classes_Class_Library;
+using System;
+
+namespace Inner_Classes_Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of inner class
+            MarksCalculation.CalculationHelper calculationHelper = new MarksCalculation.CalculationHelper();
+            //call the inner class's method
+            Console.WriteLine(calculationHelper.Multipy(10, 5));
+
+            //call outer class's method
+            MarksCalculation mc = new MarksCalculation();
+            Student s = new Student() { SecuredMarks = 35, MaxMarks = 50 };
+            mc.CalculatePercentage(s);
+
+            Console.WriteLine(s.Percentage);
+
+            Console.ReadKey();
+        }
+    }
+}
