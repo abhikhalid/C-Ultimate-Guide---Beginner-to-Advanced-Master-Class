4e51bbe22fb28bc73e33b871f35fcb264c480f61
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-15 17:42:02 +0600

    IDisposable

---
+++ Section_19_GC_Destructors_IDisposable/IDisposable_Class_Library/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposable_Class_Library
+{
+    public class Sample : IDisposable
+    {
+        //constructor
+        public Sample() {
+            Console.WriteLine("Database connected.");
+        }
+
+        //method
+        public void DisplayDataFromDatabase()
+        {
+            Console.WriteLine("Reading data from database");
+        }
+
+        //Dispose
+
+        //Generally, it is recommended to place the database connection and file connection closing logic in the Dispose Method.
+        public void Dispose()
+        {
+            Console.WriteLine("Database disconnected");
+        }
+    }
+}
---
+++ Section_19_GC_Destructors_IDisposable/IDisposable_Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using IDisposable_Class_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposable_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object
+            // The using block ensures that the Dispose method called at the end of the 'using' block.
+            using (Sample s = new Sample())
+            {
+                s.DisplayDataFromDatabase();
+            }
+
+            Console.WriteLine("Some other work");
+            Console.ReadKey();
+        }
+    }
+}
