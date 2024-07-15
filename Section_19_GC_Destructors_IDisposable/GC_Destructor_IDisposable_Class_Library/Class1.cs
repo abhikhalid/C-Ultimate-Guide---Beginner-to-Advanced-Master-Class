2483d63682d2579c30f78382754859c7757b5933
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-15 17:04:56 +0600

    Section 19 : Destructors

---
+++ Section_19_GC_Destructors_IDisposable/GC_Destructor_IDisposable_Class_Library/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Destructor_IDisposable_Class_Library
+{
+    public class Sample
+    {
+        //constructor
+        public Sample()
+        {
+            //file and db connection logic here
+            Console.WriteLine("File is opened");
+        }
+
+        //destructor
+        // The destructor executes automatically before deleting object from the memory.
+        // Generally, objects are deleted at the end of the application, thats when the destructor gets called before end of the application.
+        // So in real-time applications you can use the destructors in order to close the files or database connections that are open by the constructor.
+        ~Sample() 
+        { 
+            //file and db closing logic here
+            Console.WriteLine("File is closed");
+        }
+    }
+}
---
+++ Section_19_GC_Destructors_IDisposable/Program.cs
@@ -0,0 +1,15 @@
+﻿using GC_Destructor_IDisposable_Class_Library;
+using System;
+namespace Section_19_GC_Destructors_IDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of Sample class
+            Sample sample = new Sample();
+            Console.ReadKey();
+        }
+        //destructor gets called automatically.
+    }
+}
