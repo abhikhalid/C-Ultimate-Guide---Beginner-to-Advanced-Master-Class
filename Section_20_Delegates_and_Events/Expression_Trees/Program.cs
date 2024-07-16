e602567f6d5899d6769b7edd5eca76fd7b9388fd
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-16 17:48:49 +0600

    Expression Trees

---
+++ Section_20_Delegates_and_Events/Expression_Trees/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Expression_Trees
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //create object of Student class
+            Student student = new Student() { StudentID = 101, StudentName = "Khalid", Age = 15};
+
+            //create expression tree with Func
+            Expression<Func<Student, bool>> expression = s => s.Age > 12 && s.Age < 20;
+
+            //Compile expresion using Compile method to invoke it as a Delegate
+            Func<Student, bool> myDelegate = expression.Compile();
+
+            //Execute the method
+            bool result = myDelegate.Invoke(student);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
