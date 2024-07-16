13ce8321fe40fa8fd190566116f0b348c1c5be97
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2024-07-16 18:28:36 +0600

    Switch Expression

---
+++ Section_20_Delegates_and_Events/Switch_Expression_ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Expression_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int operation = 1; // 1 to 4
+            string result;
+
+            //switch expression
+            result = operation switch
+            {
+                1 => "Customer",
+                2 => "Employee",
+                3 => "Supplier",
+                4 => "Distributor",
+                _ => "No case available"
+            };
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
