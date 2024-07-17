0c2fcbcc185339c812953929188ec4c075d1657e
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 22:52:58 +0600

    Remove_RemoveAt_RemoveRange_RemoveAll_Clear

---
+++ Section_22_Collections/IndexOf_Binary_Search_Contains_ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IndexOf_Binary_Search_Contains_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Section_22_Collections/Program.cs
+++ Section_22_Collections/Program.cs
@@ -15,17 +15,34 @@ namespace Section_22_Collections
 
             //create object of List class
             //myList = new List<int>() { 10,20,30};
-            myList = new List<int>(10) { 10,20,30}; //it's better if you sure the initial size, it improves the performance.
+            myList = new List<int>(10) { 10,20,30,40,50}; //it's better if you sure the initial size, it improves the performance.
 
 
-            //insert element at position 1
-            myList.Insert(1, 100);
+            //remove 30
+            //myList.Remove(30);
 
+            //remove at Index 2
+            // myList.RemoveAt(2);
+            //myList.RemoveAt(5); // ArgumentOutOfRangeException
 
-            //insert elements at position 2
-            List<int> otherList = new List<int>() { 200, 300, 400 };
+            /*
+            int ind = 5;
+            if(ind < myList.Count)
+            {
+                myList.RemoveAt(ind);
+                Console.WriteLine("Removed");
+            }
+            */
 
-            myList.InsertRange(2, otherList);
+            //remove range starts from index 1, count is 2 elements
+            //myList.RemoveRange(1, 2);
+
+            //RemoveAll (remove all numbers > 30)
+
+            //lamda expression
+            myList.RemoveAll(n => n>=30);
+
+            myList.Clear();
 
 
             Console.WriteLine("Using foreach loop:");
