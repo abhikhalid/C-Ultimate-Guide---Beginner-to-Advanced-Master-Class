ababf9be2ff5d3bb4b95fca091e86305a1b98b1e
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 21:10:29 +0600

    List_Class

---
+++ Section_22_Collections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_22_Collections
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create reference variable for List Class
+            List<int> myList;
+
+            //create object of List class
+            //myList = new List<int>() { 10,20,30};
+            myList = new List<int>(10) { 10,20,30}; //it's better if you sure the initial size, it improves the performance.
+
+            //read elements using foreach loop
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            //read elements using for loop
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
