15548def15d34093535761f2d8fdf35bb2da6ba2
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-18 00:09:28 +0600

    ToArray_ForEach

---
+++ Section_22_Collections/ToArray_ForEach_ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToArray_ForEach_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create list
+            List<string> myFriends = new List<string>()
+            {
+                "Scott",
+                "Allen",
+                "James",
+                "Jones"
+            };
+
+            //convert to array
+            string[] myFrirendArray = myFriends.ToArray();
+
+            //display array
+            foreach (string item in myFrirendArray)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //forEach method
+            myFriends.ForEach(friend => Console.WriteLine(friend));
+
+            Console.ReadKey();
+        }
+    }
+}
