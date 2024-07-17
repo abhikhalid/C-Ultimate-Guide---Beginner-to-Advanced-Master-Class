c784b1eed19c191443cf20b4f14f1f3167720d34
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 10:59:09 +0600

    Arrays_with_for_loop

--- Section_21_Arrays/Program.cs
+++ Section_21_Arrays/Program.cs
@@ -13,19 +13,15 @@ namespace Section_21_Arrays
             string[] b = new string[5] {"one","two","three","four","five"};
 
 
-            //display the values of elements
-            Console.WriteLine(a[0]);
-            Console.WriteLine(a[1]);
-            Console.WriteLine(a[2]);
-            Console.WriteLine(a[3]);
-            Console.WriteLine(a[4]);
-            //Console.WriteLine(a[5]); // Index out of Range Exception
+            for (int i = 0; i < a.Length; i++) {
+               Console.WriteLine(a[i]);
+            }
 
-            Console.WriteLine(b[0]);
-            Console.WriteLine(b[1]);
-            Console.WriteLine(b[2]);
-            Console.WriteLine(b[3]);
-            Console.WriteLine(b[4]);
+            Console.WriteLine();
+
+            for (int i = 0; i < b.Length; i++) {
+                Console.WriteLine(b[i]);
+            }
 
 
             Console.ReadKey();
