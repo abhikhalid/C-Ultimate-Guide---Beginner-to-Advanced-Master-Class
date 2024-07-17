c10b9a9a5c94a3998501c9bcb6b3dbd4fe92e226
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 14:51:49 +0600

    Array_of_objects

---
+++ Section_21_Arrays/Arrays_Of_Obejcts_Class_Library/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Of_Obejcts_Class_Library
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+
+        public string EmpName { get; set; }
+    }
+}
---
+++ Section_21_Arrays/Arrays_Of_Objects_ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Arrays_Of_Obejcts_Class_Library;
+using System;
+
+namespace Arrays_Of_Objects_ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create object of Employee class
+            Employee employee1 = new Employee()
+            {
+                EmpID = 101,
+                EmpName = "Khalid"
+            };
+
+            Employee employee2 = new Employee()
+            {
+                EmpID = 102,
+                EmpName = "Mahmud"
+            };
+
+            Employee employee3 = new Employee()
+            {
+                EmpID = 103,
+                EmpName = "Khan"
+            };
+
+
+            //create array of objects
+            Employee[] employees = new Employee[] {employee1, employee2,employee3};
+
+            //foreach loop for array of objects
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.EmpID+" "+employee.EmpName);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
