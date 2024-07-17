451935e07bd50f564f6dabfc198f3284d175f61a
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 22:05:34 +0600

    Insert_Insesrt_Range

--- Section_22_Collections/Program.cs
+++ Section_22_Collections/Program.cs
@@ -17,21 +17,21 @@ namespace Section_22_Collections
             //myList = new List<int>() { 10,20,30};
             myList = new List<int>(10) { 10,20,30}; //it's better if you sure the initial size, it improves the performance.
 
-            //add new element at the end of existing collection
-            myList.Add(40);
 
-            //another collection
-            List<int> otherList = new List<int>() { 50, 60, 70 };
+            //insert element at position 1
+            myList.Insert(1, 100);
 
-            //adding another collection to existing collection
-            myList.AddRange(otherList);
+
+            //insert elements at position 2
+            List<int> otherList = new List<int>() { 200, 300, 400 };
+
+            myList.InsertRange(2, otherList);
 
 
             Console.WriteLine("Using foreach loop:");
-            //read elements using foreach loop
             foreach (int item in myList)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item); // 10 100 20 30
             }
 
             Console.ReadKey();
