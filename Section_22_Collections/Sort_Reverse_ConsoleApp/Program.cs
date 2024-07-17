adf6a04f557f6fc94b047816a0d64a258da2ba70
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 23:50:21 +0600

    Sort_Reverse

---
+++ Section_22_Collections/Sort_Reverse_ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Reverse_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a list
+            List<double> myNumbers = new List<double>()
+            {
+                170,150,445,120,10,4,661
+            };
+
+            //sort - asc
+            myNumbers.Sort();
+
+            //reerse - sort in desc
+            myNumbers.Reverse();
+
+            //after sorting
+            foreach (double number in myNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
