413232cbf9cea3bcbf1523e0913c471f4f22bb60
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 21:31:09 +0600

    Add_Add_Range

--- Section_22_Collections/Program.cs
+++ Section_22_Collections/Program.cs
@@ -17,20 +17,22 @@ namespace Section_22_Collections
             //myList = new List<int>() { 10,20,30};
             myList = new List<int>(10) { 10,20,30}; //it's better if you sure the initial size, it improves the performance.
 
+            //add new element at the end of existing collection
+            myList.Add(40);
+
+            //another collection
+            List<int> otherList = new List<int>() { 50, 60, 70 };
+
+            //adding another collection to existing collection
+            myList.AddRange(otherList);
+
+
+            Console.WriteLine("Using foreach loop:");
             //read elements using foreach loop
             foreach (int item in myList)
             {
                 Console.WriteLine(item);
             }
-
-            Console.WriteLine();
-
-            //read elements using for loop
-            for (int i = 0; i < myList.Count; i++)
-            {
-                Console.WriteLine(myList[i]);
-            }
-
 
             Console.ReadKey();
         }
