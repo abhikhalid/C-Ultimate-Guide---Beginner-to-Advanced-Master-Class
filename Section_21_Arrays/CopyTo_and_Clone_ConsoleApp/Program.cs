d96779fc166e00367d0190ccbdd2c25ee4d7fb79
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-17 19:41:55 +0600

    CopyTo_and_Clone

---
+++ Section_21_Arrays/CopyTo_and_Clone_ConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CopyTo_and_Clone_ConsoleApp
+{
+    class Employee
+    {
+        public string Employeename { get; set; }
+        public string Role { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[]
+            {
+                new Employee(){Employeename="Khalid",Role="Developer"},
+                new Employee(){Employeename="Mahmud",Role="Hero"},
+                new Employee(){Employeename="Khan",Role="Designer"},
+            };
+
+            //new array
+            Employee[] highlyPaidEmployees = new Employee[3];
+           
+            //Copy To
+            employees.CopyTo(highlyPaidEmployees,0);
+
+
+            //print destination array
+            foreach(Employee emp in highlyPaidEmployees)
+            {
+                if(!(emp is null))
+                {
+                    Console.WriteLine(emp.Employeename + "," + emp.Role);
+                }
+                else
+                {
+                    Console.WriteLine("null object");
+                }
+            }
+
+            Console.WriteLine();
+
+            //Clone
+            Employee[] highlyPaidEmployees2 = (Employee[])employees.Clone(); //creates a new array & copies from source array to new array
+            
+            foreach (Employee emp in highlyPaidEmployees2)
+            {
+                if (!(emp is null))
+                {
+                    Console.WriteLine(emp.Employeename + "," + emp.Role);
+                }
+                else
+                {
+                    Console.WriteLine("null object");
+                }
+            }
+
+
+            //Copy To method expects that there is an array before execution of the copy to method
+            //Clone method creates a new array by itself
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Section_21_Arrays/CopyTo_and_Clone_ConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CopyTo_and_Clone_ConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CopyTo_and_Clone_ConsoleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("10ae4b6d-83c2-404f-ab24-318f51f8db24")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
