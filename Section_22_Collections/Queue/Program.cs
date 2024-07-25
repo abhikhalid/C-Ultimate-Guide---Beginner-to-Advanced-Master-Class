35cb36c49b5513029fef9a3e09452644e5afadfe
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-25 23:10:34 +0600

    Queue

---
+++ Section_22_Collections/Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of Queue class
+            Queue<string> queue = new Queue<string>();
+
+            //Enqueue
+            queue.Enqueue("Task 3");
+            queue.Enqueue("Task 5");
+            queue.Enqueue("Task 1");
+            queue.Enqueue("Task 4");
+            queue.Enqueue("Task 2");
+
+
+            //foreach
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            string dq = queue.Dequeue();
+            Console.WriteLine("Dequeue:" + dq);
+
+            //Peek 
+            string pk = queue.Peek();
+            Console.WriteLine("Peek : " + pk);
+
+            Console.ReadKey();
+        }
+    }
+}
