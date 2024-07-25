cab7532650ca3f01e0d6aa3ef0c3df747a8016ae
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-25 23:56:58 +0600

    Collection_of_Objects

---
+++ Section_22_Collections/Collection_of_Objects_ConsoleApp/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Collection_of_Objects_ConsoleApp
+{
+    /// <summary>
+    /// This is a XML tag.
+    /// Represents a Product in ECommerce application.
+    /// </summary>
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime DateOfManufacture {  get; set; }    
+    }
+}
---
+++ Section_22_Collections/Collection_of_Objects_ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection_of_Objects_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Product> products = new List<Product>();
+
+            string choice;
+
+            do
+            {
+                Console.WriteLine("Enter Product ID: ");
+                int pId = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Product Name:");
+                string pName = Console.ReadLine();
+                Console.WriteLine("Enter Price:");
+                double unitPrice = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Date of Manufacture (yyyy-MM-dd):");
+                DateTime dom = DateTime.Parse(Console.ReadLine());
+
+                Product product = new Product()
+                {
+                    ProductID = pId,
+                    ProductName = pName,
+                    Price = unitPrice,
+                    DateOfManufacture = dom
+                };
+
+                products.Add(product);
+
+                Console.WriteLine("Product Added\n");
+                Console.WriteLine("Do you want to continue to next product?");
+                choice = Console.ReadLine();    
+
+            } while (choice != "No" && choice != "no" && choice != "n");
+
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.ProductID+","+product.ProductName
+                    + ", "+product.Price + ", "+product.DateOfManufacture.ToShortDateString());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
