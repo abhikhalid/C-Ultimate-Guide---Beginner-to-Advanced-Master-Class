aa18bdbd66afbb2544df910e3638fc79269754f0
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-25 22:08:17 +0600

    Stack

---
+++ Section_22_Collections/Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Student
+    {
+        public int Marks { get; set; }
+        public int Rank { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of Stack
+            Stack<Student> marks = new Stack<Student>();
+
+            //Add
+            marks.Push(new Student() { Marks = 45});
+            marks.Push(new Student() { Marks = 61});
+            marks.Push(new Student() { Marks = 80});
+
+
+            //Pop
+            Student stu = marks.Pop();
+            Console.WriteLine("Pop: "+stu.Marks);
+
+            //Peek
+            Student student = marks.Peek();
+            Console.WriteLine("Peek : " + student.Marks);
+
+            int r = 1;
+
+            foreach (var item in marks)
+            {
+                item.Rank = r++;
+                Console.WriteLine(item.Marks + ", "+ item.Rank);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
