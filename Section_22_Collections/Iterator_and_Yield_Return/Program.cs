11bc4541d405635c4ab9c059dd191dd4195c75e4
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-26 19:27:44 +0600

    Iterator_and_Yield_Return

--- Section_22_Collections/Iterator_and_Yield_Return/Program.cs
+++ Section_22_Collections/Iterator_and_Yield_Return/Program.cs
@@ -5,12 +5,20 @@ namespace Iterator_and_Yield_Return
 {
     public class Sample
     {
-        public IEnumerable<int> Method()
+        public List<double> Prices { get; set; } = new List<double>()
         {
-            Console.WriteLine("Iterator method executes");
-            yield return 10; //yield return statement returns a value but continues the execution of that method.
-            Console.WriteLine("Iterator method executes conitued");
-            yield return 20;
+            90,34,12,80
+        };
+
+        //Iterator method
+        public IEnumerable<double> Method()
+        {
+            double sum = 0;
+            foreach (double price in Prices)
+            {
+                sum += price;
+                yield return sum; // 90,124,136,216
+            }
         }
     }
 
@@ -21,27 +29,16 @@ namespace Iterator_and_Yield_Return
         {
             Sample s = new Sample();
             var enumerable1 = s.Method();
+            var prices_enumerator = enumerable1.GetEnumerator();
 
+            prices_enumerator.MoveNext();
+            Console.WriteLine("Total price up to first product:"+prices_enumerator.Current);
+            Console.WriteLine("Some more code...");
 
-            /*
-            var enumerator1 = enumerable1.GetEnumerator();
-            enumerator1.MoveNext();
-            Console.WriteLine(enumerator1.Current);
+            prices_enumerator.MoveNext();
 
-            //DOING SOME OTHER TASK HERE
-            // ..................
-            // ...............
-            // .............
-
-            enumerator1.MoveNext();
-            Console.WriteLine(enumerator1.Current);
-            */
-
-            //ALTERNATIVELY, USE FOR EACH LOOP
-            foreach (var item in enumerable1)
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine("Total price up to second product:" + prices_enumerator.Current);
+            Console.WriteLine("Some more code...");
 
 
             Console.ReadKey();
