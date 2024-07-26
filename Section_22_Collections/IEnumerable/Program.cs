9d4818ea85377754dbc492b21474645f9621ff96
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-26 17:19:23 +0600

    IEnumerable_and_IEnumerator

---
+++ Section_22_Collections/IEnumerable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IEnumerable
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //create a collectino
+            IEnumerable<string> messages;
+            messages = new List<string>()
+            {
+                "How are you",
+                "Have a great day",
+                "Thanks for the meeting"
+            };
+
+            //foreach
+            //Console.WriteLine("IEnumerable");
+
+            //foreach (var message in messages) {
+                
+            //    Console.WriteLine(message);
+
+            //}
+
+            //Let's see how foreach works behind the scene
+            Console.WriteLine("\nIEnumerator");
+            IEnumerator<string> enumerator = messages.GetEnumerator();
+            enumerator.Reset();
+            while (enumerator.MoveNext()) { 
+              Console.WriteLine(enumerator.Current);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
