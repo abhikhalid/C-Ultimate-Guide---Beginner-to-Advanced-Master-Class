03c5b6f4d24a4e7eb1e3046147f3cd9f4e67b295
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-26 09:41:27 +0600

    Object_Relation_One_to_One

---
+++ Section_22_Collections/Object_Relation_One_To_One_Class_Library/Branch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Object_Relation_One_To_One_Class_Library
+{
+    public class Branch
+    {
+        public string BranchName { get; set; }
+
+        public int NoOfSemester { get; set; }
+    }
+}
---
+++ Section_22_Collections/Object_Relation_One_To_One_Console_App/Program.cs
@@ -0,0 +1,41 @@
+﻿using Object_Relation_One_To_One_Class_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Relation_One_To_One_Console_App
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();
+            student.RollNo = 123;
+            student.StudentName = "Khalid";
+            student.Email = "khalid@gmail.com";
+
+            //Branch br = new Branch();
+            //br.BranchName = "Computer Science Engineering";
+            //br.NoOfSemester = 8;
+
+            ////one-to-one releation
+            //student.branch = br;
+
+            //or
+            student.branch = new Branch();
+            student.branch.BranchName = "Computer Science Engineering";
+            student.branch.NoOfSemester = 8;
+
+            //display
+            Console.WriteLine(student.RollNo);
+            Console.WriteLine(student.StudentName);
+            Console.WriteLine(student.Email);
+            Console.WriteLine(student.branch.BranchName);
+            Console.WriteLine(student.branch.NoOfSemester);
+
+            Console.ReadKey();
+        }
+    }
+}
