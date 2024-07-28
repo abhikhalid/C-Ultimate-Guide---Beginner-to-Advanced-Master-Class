b848e3e1ad88b3fda41d11021cf6ae3dd127d779
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-28 18:19:07 +0600

    Anonymous_Arrays

---
+++ Section_23_Anonymous_Types/Anonymous_Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Anonymous_Arrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //create anonymous array/ implicitly typed array
+            var persons = new[]
+            {
+                new {PersonName = "Khalid", Email="khalid@gmail.com"},
+                new {PersonName = "Mahmud", Email="khalid@gmail.com"},
+                new {PersonName = "Khan", Email="khalid@gmail.com"},
+                new {PersonName = "Israr", Email="khalid@gmail.com"},
+            };
+
+            //foreach
+            foreach (var item in persons)
+            {
+                Console.Write(item.PersonName);
+                Console.Write(",");
+                Console.WriteLine(item.Email);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
