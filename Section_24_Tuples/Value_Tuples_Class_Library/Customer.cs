b2785ad205d976eab14c615b049ba2250ec16480
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-28 19:52:24 +0600

    Deconstructing

---
+++ Section_24_Tuples/Value_Tuples_Class_Library/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Value_Tuples_Class_Library
+{
+    public class Customer
+    {
+        public (int customerID,string customerName, string email) GetCustomerDetails()
+        {
+            return (101,"Khalid","khalid@gmail.com");
+        }
+    }
+}
---
+++ Section_24_Tuples/Value_Tuples_Console_App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Value_Tuples_Class_Library;
+
+namespace ClassLibrary1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();   
+
+            //get details
+            (int customerID, string customerName,string email) cust = customer.GetCustomerDetails();
+
+            Console.WriteLine(cust.customerID);
+            Console.WriteLine(cust.customerName);
+            Console.WriteLine(cust.email);
+        
+            Console.ReadKey();
+        }
+    }
+}
