4cb3bf7018472d1983b7e106328f6564bc5f83f7
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-28 19:27:01 +0600

    Tuple_Class

---
+++ Section_24_Tuples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Section_24_Tuples
+{
+    class Sample
+    {
+        public Tuple<string,int> GetPersonDetails()
+        {
+            Tuple<string, int> person = new Tuple<string, int>("Khalid", 20);
+
+            return person;
+        }
+    }
+
+    
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+
+            Tuple<string, int> person = s.GetPersonDetails();
+
+            //access values from tuple
+            Console.WriteLine(person.Item1);
+            Console.WriteLine(person.Item2);
+
+            Console.ReadKey();
+        }
+    }
+}
