bdbe9e752d71162c3de7762ca14b89c14c5d0ee2
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-30 16:44:54 +0600

    Sec_26_Note

---
+++ Section_26_String_DateTime_Math/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Section_26_String_DateTime_Math
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string x = "Hello World";
+            int n = x.Length;
+
+            char c = x[0];
+
+            Console.WriteLine(x);
+            Console.WriteLine(n);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
