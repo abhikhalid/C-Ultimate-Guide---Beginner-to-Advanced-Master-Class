586525310fcd7e6b1782852aaa3794aad1d5d327
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-30 21:06:06 +0600

    Octal_Number_System

---
+++ Section_27_IO_Serialization_Encoding/Octal_Number_System/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Octal_Number_System
+{
+    class Program
+    {
+        static void Main()
+        {
+            int dec1 = 289;
+            string oct1 = Convert.ToString(dec1,8);
+
+            Console.WriteLine(oct1);
+
+            int dec2 =  Convert.ToInt32(oct1,8);
+            Console.WriteLine(dec2);
+
+            Console.ReadKey();
+        }
+    }
+}
