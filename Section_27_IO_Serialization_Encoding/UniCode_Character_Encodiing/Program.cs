8a283b78008a0589b00742ca8eb12eb163d7d67c
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-07-31 17:10:09 +0600

    UniCode_Character_Encoding

---
+++ Section_27_IO_Serialization_Encoding/UniCode_Character_Encodiing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+
+namespace UniCode_Character_Encodiing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mytext = "অ";
+            Console.WriteLine(mytext);
+
+            string mytext2 = "\u0987";
+            Console.WriteLine(mytext2);
+
+
+            string sentence = "The quick brown fox jumps over the lazy dog";
+            //In real life, when writng to file, we do this
+            byte[] bytes = Encoding.Unicode.GetBytes(sentence);
+
+            string sentence2 = Encoding.Unicode.GetString(bytes);
+            Console.WriteLine(sentence2);
+
+            Console.ReadKey();
+        }
+    }
+}
