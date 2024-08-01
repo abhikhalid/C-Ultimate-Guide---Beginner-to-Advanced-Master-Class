a6d73730848cfe358df582895b06630ca94e7ab1
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-08-01 09:49:44 +0600

    FileInfo_Class_and_its_Properties

---
+++ Section_27_IO_Serialization_Encoding/FileInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace FileInfo1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string filePath = "D:\\Coding\\Development\\C#\\C_Sharp-Ultimate-Guide-Beginner-to-Advanced-Master-Class\\Section_27_IO_Serialization_Encoding\\FileInfo\\japan.txt";
+            string destPath = "D:\\Coding\\Development\\C#\\C_Sharp-Ultimate-Guide-Beginner-to-Advanced-Master-Class\\Section_27_IO_Serialization_Encoding\\FileInfo\\another.txt";
+            string destPath2 = "D:\\Coding\\Development\\C#\\C_Sharp-Ultimate-Guide-Beginner-to-Advanced-Master-Class\\Section_27_IO_Serialization_Encoding\\FileInfo\\somethingelse.txt";
+
+            FileInfo fileInfo = new FileInfo(filePath);
+           
+            //Create()
+            fileInfo.Create().Close();
+            Console.WriteLine("japan.txt created");
+
+            //CopyTo()
+            FileInfo fileInfo2 = fileInfo.CopyTo(destPath);
+            Console.WriteLine(fileInfo2.Name + " created.");
+
+
+            //MoveTo()
+            fileInfo2.MoveTo(destPath2);
+            Console.WriteLine(fileInfo2.Name + "moved");
+
+            //Delete()
+            fileInfo2.Delete();
+            Console.WriteLine(fileInfo2.Name + " deleted");
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Section_27_IO_Serialization_Encoding/File_Info_Class_Properties/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace File_Info_Class_Properties
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string filePath = "D:\\Coding\\Development\\C#\\C_Sharp-Ultimate-Guide-Beginner-to-Advanced-Master-Class\\Section_27_IO_Serialization_Encoding\\File_Info_Class_Properties\\japan.txt";
+
+            FileInfo fileInfo = new FileInfo(filePath);
+        
+            Console.WriteLine("Exists:"+fileInfo.Exists);
+            
+            if(fileInfo.Exists )
+            {
+                Console.WriteLine("FullName: " + fileInfo.FullName);
+                Console.WriteLine("Name:" + fileInfo.Name);
+                Console.WriteLine("DirectoryName:" + fileInfo.DirectoryName);
+                Console.WriteLine("Extension:" + fileInfo.Extension);
+                Console.WriteLine("Length:" + fileInfo.Length);
+                Console.WriteLine("LastWriteTime:" + fileInfo.LastWriteTime);
+                Console.WriteLine("LastAccessTime:" + fileInfo.LastAccessTime);
+                Console.WriteLine("CreationTime:" + fileInfo.CreationTime);
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+         
+            Console.ReadKey();
+        }
+    }
+}
