6e316a6aae7b9dc62bdbcece5b7d55b4d33928cd
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-08-01 20:31:56 +0600

    FileStream_Class_Part_2

--- Section_27_IO_Serialization_Encoding/FileStream_Class/Program.cs
+++ Section_27_IO_Serialization_Encoding/FileStream_Class/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace FileStream_Class
 {
@@ -7,19 +8,40 @@ namespace FileStream_Class
     {
         static void Main()
         {
-            string filePath = @"D:\Coding\Development\C#\C_Sharp-Ultimate-Guide-Beginner-to-Advanced-Master-Class\Section_27_IO_Serialization_Encoding\FileStream_Class";
+            string filePath = @"D:\Coding\Development\C#\C_Sharp-Ultimate-Guide-Beginner-to-Advanced-Master-Class\Section_27_IO_Serialization_Encoding\FileStream_Class\dog.txt";
 
 
             //FileStream fileStream = new FileStream(filePath,FileMode.Create,FileAccess.Write);
             //FileStream fileStream = File.Create(filePath);
             //FileStream fileStream = File.Open(filePath,FileMode.Create,FileAccess.Write);
             //FileStream fileStream = File.OpenWrite(filePath);
-            
+
             //or
 
             FileInfo fileInfo = new FileInfo(filePath);
-            FileStream fileStream = fileInfo.Open(FileMode.CreateNew,FileAccess.Write);
+            FileStream fileStream = fileInfo.Open(FileMode.Create, FileAccess.Write);
             //FileStream fileStream = fileInfo.OpenWrite();
+
+            //create content
+            string content = "Dog is one of the domestic animal";
+            byte[] bytes = Encoding.ASCII.GetBytes(content);
+
+            //Write
+            fileStream.Write(bytes, 0, bytes.Length);
+
+
+            //some other work here (assume 100 lines of code)
+
+            string content2 = "other text here";
+            byte[] bytes2 = Encoding.ASCII.GetBytes(content2);
+
+            fileStream.Write(bytes2 , 0, bytes2.Length);
+
+            fileStream.Close(); // in order to save the file properly, we have to close the stream.
+
+            Console.WriteLine("dog.txt created");
+
+            Console.ReadKey();
         }
     }
 }
