8d0feed275ad3ed48852154eb5821a1a52e12896
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-08-01 15:07:50 +0600

    DriveInfo_Clas

---
+++ Section_27_IO_Serialization_Encoding/DriveInfo_Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace DriveInfo_Class
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DriveInfo driveInfo = new DriveInfo("c");
+            
+            Console.WriteLine("Name: "+driveInfo.Name);
+            Console.WriteLine("Drive Type: "+driveInfo.DriveType);
+            Console.WriteLine("Volume Label: "+driveInfo.VolumeLabel);
+            Console.WriteLine("Root Directory:"+driveInfo.RootDirectory);
+            Console.WriteLine("Total Size: "+(driveInfo.TotalSize/1024/1024/1024));
+            Console.WriteLine("Free space: "+(driveInfo.AvailableFreeSpace/1024/1024/1024)+" GB");
+        
+            Console.ReadKey();
+        }
+    }
+}
