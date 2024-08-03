0a7a49cb224970b2643ebfc5bd563778fb345e55
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-08-03 13:38:20 +0600

    Section_28_Exception_Handling_Try_Catch_Finally

---
+++ Section_28_Exception_Handling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Section_28_Exception_Handling
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int a, b;
+
+                Console.Write("Enter First Number : ");
+                string input1 = Console.ReadLine();
+                a = int.Parse(input1);
+
+                Console.Write("Enter Second Number:");
+                string input2 = Console.ReadLine();
+                b = int.Parse(input2);
+
+                int c = a / b; // b = 0; it raises 'DivideByZero' exception.
+                Console.WriteLine("Result of Division:" + c);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally //no matter exception is raised or not faised, finally block will exceute.
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
