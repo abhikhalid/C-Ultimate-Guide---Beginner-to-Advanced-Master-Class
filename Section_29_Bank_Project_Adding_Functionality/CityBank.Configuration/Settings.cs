c4ef2f56f01661cbdb95653c670a881b9367edd2
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-08-07 18:54:24 +0600

    Adding_Configuration_Settings

---
+++ Section_29_Bank_Project_Adding_Functionality/CityBank.Configuration/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityBank.Configuration
+{
+    /// <summary>
+    /// Project level configuration settings
+    /// </summary>
+    public static class Settings
+    {
+        /// <summary>
+        /// Customer number starts from 10001; incremented by 1
+        /// </summary>
+        public static long BaseCustomerNo { get; set; } = 1000;
+    }
+}
