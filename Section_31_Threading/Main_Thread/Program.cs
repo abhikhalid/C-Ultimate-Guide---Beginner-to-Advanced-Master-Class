dd7aac1880d392119c1842d04590c3dc3487e9f4
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-08-08 13:31:41 +0600

    Thread_Class

--- Section_31_Threading/Main_Thread/Program.cs
+++ Section_31_Threading/Main_Thread/Program.cs
@@ -14,11 +14,22 @@ namespace Main_Thread
             Thread mainThread = Thread.CurrentThread;
             mainThread.Name = "Main Thread";
 
+            ThreadPriority priority = mainThread.Priority;
+
             Console.WriteLine(mainThread);
-            Console.WriteLine(mainThread.Name);
+            Console.WriteLine(mainThread.Name); // Main Thread
 
-            Method1();
+            Console.WriteLine(priority.ToString()); // Normal
 
+            Console.WriteLine(mainThread.IsAlive); // True
+
+            Console.WriteLine(mainThread.IsBackground); // False
+
+            ThreadState state = mainThread.ThreadState;
+            Console.WriteLine(state.ToString()); // Running
+
+
+            //Method1();
             Console.ReadKey();
         }
 
