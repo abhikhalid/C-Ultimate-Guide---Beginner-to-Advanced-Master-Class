abd98e970473583b2927156790109518cf9243c7
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-08-08 12:39:31 +0600

    Main_Thread

--- Section_31_Threading/Main_Thread/Program.cs
+++ Section_31_Threading/Main_Thread/Program.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Main_Thread
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main Thread";
+
+            Console.WriteLine(mainThread);
+            Console.WriteLine(mainThread.Name);
+
+            Method1();
+
+            Console.ReadKey();
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method 1");
         }
     }
 }
