4e96cfa40ceca8b8b2b925b7bdb91d8707b969f4
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-11-10 15:38:36 +0600

    Nullable_Reference_Type

---
+++ Section 30 - C Sharp 9 and 10 - New Features/Nullable Reference Types/ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿class Employee
+{
+    public int x { get; set; }
+}
+
+class EmployeeBusinessLogic
+{
+    public Employee? GetEmployee()
+    {
+        //return new Employee()
+        //{
+        //    x = 10
+        //};
+
+        return null;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
+        Employee? employee = employeeBusinessLogic.GetEmployee();
+
+        if(employee != null)
+        {
+            Console.WriteLine(employee.x);
+        }
+
+        //Console.WriteLine(employee.x);
+        Console.ReadKey();
+    }
+}
+
+//Employee  = Non-nullable (null values are not accepted!)
+//Employee? = Nullable (accepts null value)
