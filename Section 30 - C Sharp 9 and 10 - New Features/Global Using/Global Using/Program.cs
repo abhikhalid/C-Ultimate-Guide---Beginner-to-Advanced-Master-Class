30dee70cf509f57ced1f98b74a688a9832f63b14
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-11-10 12:51:15 +0600

    Global_Using

---
+++ Section 30 - C Sharp 9 and 10 - New Features/Global Using/Global Using/Program.cs
@@ -0,0 +1,9 @@
+﻿//here we do not need to import any commmon namespace, because we already globally imported those namespace in 'GlobalUsing.cs' file.
+
+List<int> numbers = new List<int> { 1, 2, 3 }; 
+List<int> result =  numbers.Where(temp => temp > 0).ToList();
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
