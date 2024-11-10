4337d7a90cd5e995158d2aabd8ba39a693646947
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-11-10 11:29:36 +0600

    Section_30_C_Sharp_9_and_10

---
+++ Section 30 - C Sharp 9 and 10 - New Features/TopLevelStatement/TopLevelStatement/Program.cs
@@ -0,0 +1,19 @@
+﻿// we can also import namespaces
+using System;
+using System.IO;
+
+
+Console.WriteLine("Hello, World!");
+
+string name = "Khalid";
+Console.WriteLine($"Hello to {name}");
+Console.ReadKey();
+
+// we can mix namespace with classes
+namespace namespace1
+{
+    class Sample
+    {
+
+    }
+}
