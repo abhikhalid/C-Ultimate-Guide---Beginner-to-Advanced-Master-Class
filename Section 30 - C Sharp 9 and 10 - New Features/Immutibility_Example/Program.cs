fab547315fda68e8d702bb031e4a6c1c2542cbf8
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-12-01 11:44:51 +0600

    Need_of_Immutability

---
+++ Section 30 - C Sharp 9 and 10 - New Features/Immutibility_Example/Program.cs
@@ -0,0 +1,38 @@
+﻿class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public double CurrentBalance { get; set; }
+}
+
+class DataStorage
+{
+    // Developer 1 (YOU)
+    public static List<BankAccount> GetBankAccounts()
+    {
+        return new List<BankAccount>()
+        {
+            new BankAccount(){AccountNumber = 1, CurrentBalance = 1000},
+            new BankAccount(){AccountNumber = 2, CurrentBalance = 20000}
+        };
+    }
+    
+    //Deverloper 2
+    public static double GetCurrentBalance(BankAccount bankAccount)
+    {
+        bankAccount.AccountNumber = 100; // unexpectedly it changes the value of Account Number
+        return bankAccount.CurrentBalance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+       List<BankAccount> bankAccounts =  DataStorage.GetBankAccounts();
+       BankAccount firstBankAccount = bankAccounts[0];
+
+       double bal = DataStorage.GetCurrentBalance(firstBankAccount);
+       Console.WriteLine(firstBankAccount.AccountNumber + "," + bal);
+       Console.ReadKey();
+    }
+}
