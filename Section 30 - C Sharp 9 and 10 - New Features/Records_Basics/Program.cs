e0f527dc58d780cfbb261b868a258d2f7671bcb3
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-12-01 14:44:36 +0600

    Records_Immutability

--- Section 30 - C Sharp 9 and 10 - New Features/Records_Basics/Program.cs
+++ Section 30 - C Sharp 9 and 10 - New Features/Records_Basics/Program.cs
@@ -1,16 +1,28 @@
 ﻿namespace records_example
 {
-    public record Person(string Name, int Age,Address PersonAddress);
-    public record Address(string city);
+    //by default records are immutable.
+    public record Person(string Name, int Age, Address PersonAddress)
+    {
+        //but, in this way you can add mutable properties.
+        public int x { get; set; }
+    }
+
+    // Whenever you would like to create fully immutable properties records are best or 
+    // you want all immutable properties with just one or two mutable properties for some reason then also records are best!
+
+    public record Address(string city)
+    {
+        public string? Country { get; set;}
+    }
 
 
     class Program
     {
         static void Main()
         {
-            //Person person = new Person("John",20,100); //error
             Person person1 = new Person("John", 20, new Address("Dhaka"));
             Person person2 = new("Khalid", 20,new Address("Pabna"));
+            person2.PersonAddress.Country = "Bangladesh"; //setting mutable property. 
 
             Console.WriteLine($"{person1.Name},{person1.Age},{person1.PersonAddress.city}");
             Console.WriteLine($"{person2.Name},{person2.Age},{person2.PersonAddress.city}");
