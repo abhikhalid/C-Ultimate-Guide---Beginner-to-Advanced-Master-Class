be6a77fe87ca1ba6283e3da254a08bb97a266a30
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-12-22 23:49:15 +0600

    Interface_Default_Methods

---
+++ Section 30 - C Sharp 9 and 10 - New Features/Interface_Default_Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace interface_enhancements
+{
+    public interface IEmployee
+    {
+        public string Name { get; set; }
+
+        //default interface methods or virtual extension method
+        public string GetNameInUpperCase()
+        {
+            return Name.ToUpper();
+        }
+    }
+
+    public class Manager: IEmployee
+    {
+        private string _name;
+
+        public string Name { get; set; }
+    }
+
+    public class Clert: IEmployee
+    {
+        private string _name;
+
+        public string Name { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Manager manager = new Manager();
+            manager.Name = "Khalid";
+            //Console.WriteLine(manager.Name);
+            IEmployee iemp = (IEmployee)manager;
+
+            Console.WriteLine(iemp.GetNameInUpperCase());
+            Console.ReadKey();
+        }
+    }
+}
+
