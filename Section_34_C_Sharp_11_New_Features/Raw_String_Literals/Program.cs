cba4b59328582671ad9185a8269cf461a1e1fd67
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-12-25 12:09:21 +0600

    Sec_34_C_Sharp_11_Net_Features

---
+++ Section_34_C_Sharp_11_New_Features/Raw_String_Literals/Program.cs
@@ -0,0 +1,21 @@
+﻿//{
+//    "name": "Khalid Mahmud",
+//    "age": 30
+//}
+
+string jsonString = "{\n    \"name\": \"Khalid Mahmud\",\n    \"age\": 30\n}";
+
+string jsonString2 = """
+{
+    "name": "Khalid Mahmud",
+    "age": 30
+}
+""";
+
+//Console.WriteLine(jsonString);
+Console.WriteLine(jsonString2);
+
+
+
+
+Console.ReadKey();
